a94114b357cc06685d42e18b0885fe1aa9ee02ec
zem321 <Nik-Arbuzof@yandex.ru>
2026-05-31 20:05:46 +0300

    Delete states.py

--- states.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class BotStates(StatesGroup):
-    main_menu = State()
-    chat_mode = State()
-    image_generate = State()
-    image_edit = State()
