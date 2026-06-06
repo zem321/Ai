cb8e45f99fdd3c0f80da1884245d5846f07661d7
zem321 <Nik-Arbuzof@yandex.ru>
2026-06-06 15:29:15 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,8 +1,9 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
 class BotStates(StatesGroup):
     main_menu = State()
     chat_mode = State()
     image_generate = State()
     image_edit = State()
+    video_generate = State()
+    video_edit = State()
